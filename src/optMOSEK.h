ef62cf596d4501394538bc53e0e15f03b8ad0ab5
Lumorti <lbmortimer1998@gmail.com>
2025-06-19 13:25:21 +0200

    finally moved over to use the external library

--- src/optMOSEK.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "poly.h"
-
-// Convert to MOSEK form and solve
-double solveMOSEK(Poly obj, std::vector<std::vector<std::vector<Poly>>>& psd, std::vector<Poly>& constraintsZero, std::vector<Poly>& constraintsPositive, int verbosity, std::pair<int,int> varBounds = {-1, 1}, std::map<Mon, std::complex<double>>* solution = nullptr);
-
-
--- src/optOptim.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include "poly.h"
-
-double solveOptim(Poly& objective, std::vector<Poly>& cons, std::vector<std::vector<std::vector<Poly>>>& momentMatrix, std::map<Mon, std::complex<double>>& startVals, int verbosity=1, int maxIters=1000000, int numExtra=0, double distance=1, double tolerance=1e-8);
